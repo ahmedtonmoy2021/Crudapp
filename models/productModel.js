af870fae1a3af893d330f5a8f224a579fd316966
ahmedtonmoy2021 <ahmedtonmoy52@gmail.com>
2025-03-12 01:53:22 +0000

    update

--- models/productModel.js
+++ models/productModel.js
@@ -1,25 +1,31 @@
 const mongoose = require('mongoose')
-const productSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "please enter a product name"] // Validation error message
-        },
-
-        quantity:{
-            type: Number,
-            required: true,
-            default: 0
-        },
-        price:{
-            type: Number,
-            required: true,
-        }
-    },
-    {
-        timestamps: true
-    }
-)
-
-const Product = mongoose.model('product',productSchema);
-module.exports = Product;
+ 
+ const productSchema = mongoose.Schema(
+     {
+         name: {
+             type: String,
+             required: [true, "Please enter a product name"]
+         },
+         quantity: {
+             type: Number,
+             required: true,
+             default: 0
+         },
+         price: {
+             type: Number,
+             required: true,
+         },
+         image: {
+             type: String,
+             required: false,
+         }
+     },
+     {
+         timestamps: true
+     }
+ )
+ 
+ 
+ const Product = mongoose.model('Product', productSchema);
+ 
+ module.exports = Product;
